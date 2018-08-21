8c14302ab2494b5cfd8e27c03104ca0d5a5a823e
ninabreznik <ninabreznik@gmail.com>
2018-08-21 02:18:38 +0100

    Working version

--- src/index.js
+++ src/index.js
@@ -13,47 +13,74 @@ function displayArrayInput({theme, type}) {
   return bel`${displayRightType(theme, type)}`
 }
 
-// @TODO Add logic for pairs etc. of arrays (int8[2][]), now only logic for arryas of single values
-
 function displayRightType (theme, type) {
   var css = theme.classes
-  var inputField
-  if (type.includes("int")) {
-    return bel`<div class=${css.arrayContainer}>${iconsPlusMinus(theme, type)} ${inputInteger({ theme })}</div>`
+  var col = theme.colors
+  var container = bel`<div class=${css.arrayContainer}></div>`
+  var arr = getParsedArray(type) // uint8[2][3][] returns  ['', 3, 2]
+  var background = [
+    col.white,
+    col.turquoise,
+    col.lavenderGrey,
+    col.violetRed,
+    col.aquaMarine
+  ]
+  var count = 0
+  next({ container, arr })
+  return container
+
+
+  function next ({ container, arr }) {
+    var len = arr.shift()
+    if (len === '') {
+      len = 1
+      container.appendChild(plusminus({ container, arr }))
+    }
+    for (var i = 0; i < len; i++) append({ container, arr: [...arr] })
   }
-  if (type.includes("byte")) {
-    return bel`<div class=${css.arrayContainer}>${iconsPlusMinus(theme, type)} ${inputString({ theme })}</div>`
+
+  function append ({ container, arr }) {
+    if (arr.length) { // recursive step
+      var innerContainer = bel`<div class="${css.arrayContainer}"></div>`
+      container.appendChild(innerContainer)
+      next({ container: innerContainer, arr })
+    } else { // final step (stop recursion)
+      container.appendChild(returnInputFields(theme, type))
+    }
   }
-  if (type.includes("bool")) {
-    return bel`<div class=${css.arrayContainer}> ${iconsPlusMinus(theme, type)} ${inputBoolean({ theme })}</div>`
+
+  function plusminus ({ container, arr }) {
+    var css = theme.classes
+    return bel`<div class=${css.arrayPlusMinus}>
+        <i class="${css.arrayMinus} fa fa-minus" onclick=${e=>removeLast(container)}></i>
+        <i class="${css.arrayPlus} fa fa-plus" onclick=${e=>append({ container, arr: [...arr] })}></i>
+      </div>`
   }
-  if (type.includes("uint")) {
-    return bel`<div class=${css.arrayContainer}>${iconsPlusMinus(theme, type)} ${inputInteger({ theme })}</div>`
+
+  function removeLast (node) {
+    if (node.children.length > 2) node.removeChild(node.lastChild)
   }
-  if (type.includes("address")) {
-    return bel`<div class=${css.arrayContainer}>${iconsPlusMinus(theme, type)} ${inputAddress({ theme })}</div>`
-  }
+
 }
 
-function iconsPlusMinus (theme, type) {
-  var css = theme.classes
-  return bel`<div>
-      <i class="${css.arrayMinus} fa fa-minus" onclick=${e=>removeLast(e)}></i>
-      <i class="${css.arrayPlus} fa fa-plus" onclick=${e=>addNew({ e, type, theme })}></i>
-    </div>`
+function returnInputFields (theme, type) {
+  if (type.includes("int")) return inputInteger({ theme, type })
+  else if (type.includes("byte")) return inputString({ theme, type })
+  else if (type.includes("string")) return inputString({ theme, type })
+  else if (type.includes("bool")) return inputBoolean({ theme, type })
+  else if (type.includes("fixed")) return inputInteger({ theme, type })
+  else if (type.includes("address")) return inputAddress({ theme, type })
 }
 
-
-function removeLast (e) {
-  var node = e.target.parentNode.parentNode
-  if (node.children.length > 2) node.removeChild(node.lastChild)
-}
-
-function addNew ({ e, type, theme }) {
-  var node = e.target.parentNode.parentNode
-  if (type.includes("int")) node.appendChild(inputInteger({ theme }))
-  if (type.includes("byte")) node.appendChild(inputString({ theme }))
-  if (type.includes("bool")) node.appendChild(inputBoolean({ theme }))
-  if (type.includes("uint")) node.appendChild(inputInteger({ theme }))
-  if (type.includes("address")) node.appendChild(inputAddress({ theme }))
+function getParsedArray (type) {
+  var arr = []
+  var i = type.search(/\[/) // find where array starts (bool[2][])
+  var basicType = type.split('[')[0] // split to get basic type (bool, uint8)
+  var suffix = type.slice(i) // slice to get the remaining part = suffix ([2][][][])
+  suffix.split('][').forEach((x, i)=>{
+    if (x.search(/\d/) != -1) { arr.push(x.charAt(x.search(/\d/))) }  // if digit is present, push the digit
+    else { arr.push('') } // if no, push empty string
+  })
+  arr = arr.reverse()
+  return arr
 }
