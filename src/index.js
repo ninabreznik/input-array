f391254d61471586e2b08fe69f4805272c5f68e6
ninabreznik <ninabreznik@gmail.com>
2018-11-20 05:02:58 +0100

    Add validation cb

--- src/index.js
+++ src/index.js
@@ -4,56 +4,43 @@ var inputAddress = require("input-address")
 var inputInteger = require("input-integer")
 var inputBoolean = require("input-boolean")
 var inputString = require("input-string")
-
+var validator = require('solidity-validator')
 
 module.exports = displayArrayInput
 
-function displayArrayInput({theme, type}) {
-  var css = theme.classes
-  return bel`${displayRightType(theme, type)}`
-}
-
-function displayRightType (theme, type) {
+function displayArrayInput ({theme, type, cb}) {
   var css = theme.classes
   var col = theme.colors
   var container = bel`<div class=${css.arrayContainer}></div>`
   var arr = getParsedArray(type) // uint8[2][3][] returns  ['', 3, 2]
-  var background = [
-    col.white,
-    col.turquoise,
-    col.lavenderGrey,
-    col.violetRed,
-    col.aquaMarine
-  ]
-  var count = 0
-  next({ container, arr })
+  next({ container, arr, cb })
   return container
 
 
-  function next ({ container, arr }) {
+  function next ({ container, arr, cb }) {
     var len = arr.shift()
     if (len === '') {
       len = 1
-      container.appendChild(plusminus({ container, arr }))
+      container.appendChild(plusMinus({ container, arr, cb }))
     }
-    for (var i = 0; i < len; i++) append({ container, arr: [...arr] })
+    for (var i = 0; i < len; i++) append({ container, arr: [...arr], cb })
   }
 
-  function append ({ container, arr }) {
+  function append ({ container, arr, cb }) {
     if (arr.length) { // recursive step
       var innerContainer = bel`<div class="${css.arrayContainer}"></div>`
       container.appendChild(innerContainer)
-      next({ container: innerContainer, arr })
+      next({ container: innerContainer, arr, cb })
     } else { // final step (stop recursion)
-      container.appendChild(returnInputFields(theme, type))
+      container.appendChild(returnInputFields(theme, type, cb))
     }
   }
 
-  function plusminus ({ container, arr }) {
+  function plusMinus ({ container, arr, cb }) {
     var css = theme.classes
     return bel`<div class=${css.arrayPlusMinus}>
         <i class="${css.arrayMinus} fa fa-minus" onclick=${e=>removeLast(container)}></i>
-        <i class="${css.arrayPlus} fa fa-plus" onclick=${e=>append({ container, arr: [...arr] })}></i>
+        <i class="${css.arrayPlus} fa fa-plus" onclick=${e=>append({ container, arr: [...arr], cb })}></i>
       </div>`
   }
 
@@ -63,13 +50,13 @@ function displayRightType (theme, type) {
 
 }
 
-function returnInputFields (theme, type) {
-  if (type.includes("int")) return inputInteger({ theme, type })
-  else if (type.includes("byte")) return inputString({ theme, type })
-  else if (type.includes("string")) return inputString({ theme, type })
-  else if (type.includes("bool")) return inputBoolean({ theme, type })
-  else if (type.includes("fixed")) return inputInteger({ theme, type })
-  else if (type.includes("address")) return inputAddress({ theme, type })
+function returnInputFields (theme, type, cb) {
+  if (type.includes("int")) return inputInteger({ theme, type, cb })
+  else if (type.includes("byte")) return inputString({ theme, type, cb })
+  else if (type.includes("string")) return inputString({ theme, type, cb })
+  else if (type.includes("bool")) return inputBoolean({ theme, type, cb })
+  else if (type.includes("fixed")) return inputInteger({ theme, type, cb })
+  else if (type.includes("address")) return inputAddress({ theme, type, cb })
 }
 
 function getParsedArray (type) {
